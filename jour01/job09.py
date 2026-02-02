c988029f89d33f8e795f72d76d0717c90d3ef711
nicolas-riera <nicolas.riera@laplateforme.io>
2026-02-02 12:22:35 +0100

    Completed Job09

--- jour01/job09.py
+++ jour01/job09.py
@@ -8,10 +8,33 @@ class Produit:
         return self.prixHT * (self.TVA / 100) + self.prixHT
     
     def afficher(self):
-        print(f"Nom : {self.nom}")
-        print(f"Prix HT : {self.prixHT}")
-        print(f"TVA : {self.TVA}")
-        print(f"Prix TTC : {self.CalculerPrixTTC()}")
+        return [self.nom, self.prixHT, self.TVA, self.CalculerPrixTTC()]
+    
+    def changerNom(self, nom):
+        self.nom = nom
+
+    def changerPrixHT(self, prixHT):
+        self.prixHT = prixHT
+    
+    def afficherNom(self):
+        return self.nom
+    
+    def afficherPrixHT(self):
+        return self.prixHT
+    
+    def afficherTVA(self):
+        return self.TVA
 
 produit1 = Produit("Produit 1", 100, 20)
-produit1.afficher()
+print(produit1.CalculerPrixTTC())
+print(f"Nom : {produit1.afficher()[0]}, Prix HT : {produit1.afficher()[1]}, TVA : {produit1.afficher()[2]}, Prix TTC {produit1.afficher()[3]}")
+produit1.changerNom("Produit 11")
+produit1.changerPrixHT(250)
+print(f"Nom : {produit1.afficher()[0]}, Prix HT : {produit1.afficher()[1]}, TVA : {produit1.afficher()[2]}, Prix TTC {produit1.afficher()[3]}")
+
+produit2 = Produit("Produit 2", 38, 15)
+print(produit2.CalculerPrixTTC())
+print(f"Nom : {produit2.afficher()[0]}, Prix HT : {produit2.afficher()[1]}, TVA : {produit2.afficher()[2]}, Prix TTC {produit2.afficher()[3]}")
+produit2.changerNom("Produit 22")
+produit2.changerPrixHT(12)
+print(f"Nom : {produit2.afficher()[0]}, Prix HT : {produit2.afficher()[1]}, TVA : {produit2.afficher()[2]}, Prix TTC {produit2.afficher()[3]}")
