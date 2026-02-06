22204a5a0debd84e649076e2d14eee3901be0710
nicolas-riera <nicolas.riera@laplateforme.io>
2026-02-06 10:23:25 +0100

    added interactive menu

--- jour05/ship.py
+++ jour05/ship.py
@@ -32,6 +32,43 @@ class RacingShip(Ship):
     def display_speed(self):
         print(f"La vitesse maximun est de {self.max_speed}")
 
+class Menu:
+    def main_menu(self, ship):
+        print("1. Remplacer Pièce")
+        print("2. Modifier Matériau")
+        print("3. Afficher état bâteau")
+        print("4. Quitter")
+        print(f"Bâteau choisi : {ship.name}")
+
+        match input():
+            case "1":
+                return self.replace_part_menu(ship)
+            case "2":
+                return self.change_part_menu(ship)
+            case "3":
+                return self.display_state_menu(ship)
+            case "4":
+                return
+            case _:
+                return self.main_menu(ship)
+            
+    def replace_part_menu(self, ship):
+        part_name = input("Entrez le nom de la pièce que vous voulez remplacer : ")
+        new_part = Part(input("Entrez le nom de la nouvelle pièce : "), input("Entrez le matériau de la nouvelle pièce : "))
+        ship.replace_part(part_name, new_part)
+        return self.main_menu(ship)
+    
+    def change_part_menu(self, ship):
+        part_name = input("Entrez le nom de la pièce : ")
+        new_material = input("Entrez le nom du nouveau matériau : ")
+        ship.change_part(part_name, new_material)
+        return self.main_menu(ship)
+
+    def display_state_menu(self, ship):
+        ship.display_state()
+        input("Appuyez sur Entrer pour continuer")
+        return self.main_menu(ship)
+
 # Example test
 
 part1 = Part("devant", "Bois")
@@ -48,12 +85,15 @@ dict = {
     part5.name: part5,
 }
 
-s=Ship("Navire", dict)
+s = Ship("Navire", dict)
 
-s.change_part("devant", "TestBois")
-s.replace_part("DdD", part2)
+# s.change_part("devant", "TestBois")
+# s.replace_part("DdD", part2)
 
-s.display_state()
+# s.display_state()
 
-s2 = RacingShip("Narive de course", dict, 180)
-s2.display_speed()
+# s2 = RacingShip("Narive de course", dict, 180)
+# s2.display_speed()
+
+menu = Menu()
+menu.main_menu(s)
